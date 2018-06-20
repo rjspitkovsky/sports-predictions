4b626da70e59dc87e7125661901543e3f67cbd24
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-20 11:40:52 -0400

    connected rails server with react server

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello React! If this works I should see this text on my screen</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
