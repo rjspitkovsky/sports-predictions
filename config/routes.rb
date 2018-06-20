0599aa639e0d917e852aae1136e9cdeeee21e122
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-20 11:22:08 -0400

    added routes to connect with controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :predictions
+    get '/correct', to: 'predictions#correct'
+    get '/wrong', to: 'predictions#wrong'
+  end
 end
