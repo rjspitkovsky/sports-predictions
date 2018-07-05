2fad04c92ff81ea2a27cb60c3e4e92070422ee9c
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-05 11:54:31 -0400

    added created at datetime to prediction component

--- client/src/components/Prediction.js
+++ client/src/components/Prediction.js
@@ -4,6 +4,17 @@ import '../stylesheets/prediction.css';
 import { Link } from 'react-router-dom';
 
 const Prediction = ({prediction, key}) => {
+  {/*reformating the date*/}
+  const createdAtTimestamp = (prediction.created_at)
+  const createdAt = new Date(createdAtTimestamp)
+  const date = createdAt.toLocaleString(undefined, {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  })
 
 
   return (
@@ -13,6 +24,7 @@ const Prediction = ({prediction, key}) => {
       <h1>{prediction.content}</h1><br />
       <p>{prediction.sport}</p><br />
       <p>{prediction.status}</p><br />
+      <p>{date}</p><br />
       <Buttons prediction={prediction} />
       </div>
 
